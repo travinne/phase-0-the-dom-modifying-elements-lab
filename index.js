c294b56bfb595dd11bcef84b3be525997f46c7e4
travinne <travinne.mutahi@moringaschool.com>
2025-03-21 18:19:07 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
